662bf48cdbc83e74a65e9e2f0f2c6f0b5ecd02e1
przemykomo <przemyslawsowa15@gmail.com>
2019-05-04 16:40:44 +0200

    Fixed button, now it's working.

--- src/input/InputHandler.cpp
+++ src/input/InputHandler.cpp
@@ -11,16 +11,15 @@ void InputHandler::processInput(GLFWwindow* window) {
 
 void InputHandler::processCursor(GLFWwindow* window, double xPos, double yPos) {
     cursorX = xPos / GameWindow::WIDTH * 2 - 1;
-    cursorY = yPos / GameWindow::HEIGHT * 2 - 1;
+    cursorY = -(yPos / GameWindow::HEIGHT * 2 - 1);
 }
 
 void InputHandler::processMouseClick(GLFWwindow* window, int button, int action, int mods) {
     if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-        if(cursorX < Renderer::buttonPosX + Renderer::bWIDTH / 2 &&
-            cursorX > Renderer::buttonPosX - Renderer::bWIDTH / 2 &&
-            cursorY < Renderer::buttonPosY + Renderer::bHEIGHT / 2 &&
-            cursorY > Renderer::buttonPosY - Renderer::bHEIGHT / 2) {
-                std::cout << "clicked" << '\n';
+        if(cursorX < Renderer::buttonPosX + Renderer::bWIDTH / 2.0F &&
+            cursorX > Renderer::buttonPosX - Renderer::bWIDTH / 2.0F &&
+            cursorY < Renderer::buttonPosY + Renderer::bHEIGHT / 2.0F &&
+            cursorY > Renderer::buttonPosY - Renderer::bHEIGHT / 2.0F) {
                 Renderer::buttonPosX = float(rand()) / float(RAND_MAX) * 1.8F - 0.9F;
                 Renderer::buttonPosY = float(rand()) / float(RAND_MAX) * 1.8F - 0.9F;
             }
