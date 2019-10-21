e5252900f6be242327216f6cfb9f644b7bfff79a
Lukasz <lukaszm210@gmail.com>
2019-10-21 22:50:06 +0200

    .

--- build/glm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lukasz/projects/cxx/przyciski-souls-3/glm
-# Build directory: /home/lukasz/projects/cxx/przyciski-souls-3/build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
--- build/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lukasz/projects/cxx/przyciski-souls-3/glm/glm
-# Build directory: /home/lukasz/projects/cxx/przyciski-souls-3/build/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
